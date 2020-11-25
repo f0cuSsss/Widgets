35c6e75e41d2022e57e6529e880ca0e2a329f393
f0cuS <f0cuSmcsza@gmail.com>
2020-11-25 10:07:03 +0200

    Accordion widget

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import SearchWIKI from './components/SearchWIKI'
+import Accordion from './components/Accordion'
+
+
+const AccordionItems = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
 
 export default () => {
     return (
         <div>
-            <SearchWIKI />
+            <br/>
+            <Accordion items={AccordionItems}/>
         </div>
     );
 }
